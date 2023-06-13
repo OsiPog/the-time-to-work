cfad23448423ebc6bc5d32d5193ddcd9119853e0
OsiPog <osibluber@protonmail.com>
2023-06-13 19:29:59 +0200

    feat: config now saves overtime data

--- script/config_loader.js
+++ script/config_loader.js
@@ -8,7 +8,6 @@ const getConfig = async() => {
     encrypted = encrypted.config;
 
     let decrypted = vigenere(encrypted, "time-to-work", true);
-    console.log(decrypted)
 
     if (!decrypted || !encrypted || encrypted === "" || decrypted === "") decrypted = "{}"
     
@@ -16,8 +15,12 @@ const getConfig = async() => {
 
     config.started_at = config.started_at || -1
     config.history = config.history || []
-    config.overtime_sum = config.overtime_sum || 0
-    config.overtime_limit = config.overtime_limit || 144000
+    config.overtime = config.overtime || {
+        threshold: 144000, // 40 hours
+        sum: 0,
+        sum_expiration_date: 0
+    }
+
 
     return config;
 }
